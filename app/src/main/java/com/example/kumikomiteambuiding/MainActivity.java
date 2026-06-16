3648caf7ec95915bcf5115941d62df1c505a3a94
ak-2302 <s2211879@s.tsukuba.ac.jp>
2026-06-16 15:22:50 +0900

    画面にIPアドレスを表示

--- app/src/main/java/com/example/kumikomiteambuiding/MainActivity.java
+++ app/src/main/java/com/example/kumikomiteambuiding/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final String WIFI_INTERFACE_PREFIX = "wlan";
 
     private HttpAnalyzer httpAnalyzer;
+    private TextView ipAddressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        ipAddressText = findViewById(R.id.ipAddressText);
+        updateIpAddress(HttpAnalyzer.DEFAULT_PORT);
+
         httpAnalyzer = new HttpAnalyzer(this, new HttpAnalyzer.Listener() {
             @Override
             public void onDataReceived(Object data) {
@@ -37,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onServerStarted(int port) {
                 Log.i(TAG, "HTTP server started on port " + port);
+                updateIpAddress(port);
             }
 
             @Override
@@ -54,10 +67,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        int port = httpAnalyzer != null ? httpAnalyzer.getPort() : HttpAnalyzer.DEFAULT_PORT;
+        updateIpAddress(port);
+    }
+
+    @Override
     protected void onDestroy() {
         if (httpAnalyzer != null) {
             httpAnalyzer.stop();
         }
         super.onDestroy();
+    }
+
+    private void updateIpAddress(int port) {
+        String ipAddress = getLocalIpAddress();
+        if (ipAddress == null) {
+            ipAddressText.setText(R.string.ip_address_unavailable);
+            return;
+        }
+
+        ipAddressText.setText(getString(R.string.ip_address_format, ipAddress, port));
+    }
+
+    private String getLocalIpAddress() {
+        String fallbackIpAddress = null;
+
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (!networkInterface.isUp() || networkInterface.isLoopback()) {
+                    continue;
+                }
+
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (!(address instanceof Inet4Address) || address.isLoopbackAddress()) {
+                        continue;
+                    }
+
+                    String hostAddress = address.getHostAddress();
+                    if (networkInterface.getName().startsWith(WIFI_INTERFACE_PREFIX)) {
+                        return hostAddress;
+                    }
+                    if (fallbackIpAddress == null) {
+                        fallbackIpAddress = hostAddress;
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.w(TAG, "Failed to get local IP address", e);
+        }
+
+        return fallbackIpAddress;
     }
 }
