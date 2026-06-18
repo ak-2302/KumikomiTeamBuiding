6092c2158269a81bd1debddac6ca394210ac5406
ak-2302 <s2211879@s.tsukuba.ac.jp>
2026-06-18 17:52:32 +0900

    util

--- app/src/main/java/com/example/kumikomiteambuiding/Util.java
+++ app/src/main/java/com/example/kumikomiteambuiding/Util.java
@@ -6,4 +6,12 @@ public class Util {
         String tweet = URLEncoder.encode(text, StandardCharsets.UTF_8);
         return "https://twitter.com/intent/tweet?text=" + tweet;
     }
+
+    // URLを開く
+    // 使用例：openURL("https://example.com", this);
+    public static void openURL(String url, Context context) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
 }
